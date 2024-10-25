d105a7dd907263cb042b7e7f2e393e0baa7a8a57
Aliaksandr Vorakh <aliaksandrvorakh@gmail.com>
2024-10-25 22:58:31 +0200

    feat: add ws server

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 import { httpServer } from "./dist/http_server/index.js";
+import { ws_server } from "./dist/ws_server/index.js";
 
 const HTTP_PORT = 8181;
 
 console.log(`Start static http server on the ${HTTP_PORT} port!`);
 httpServer.listen(HTTP_PORT);
+
+ws_server.on('listening', () => {
+    const address = ws_server.address();
+    console.log(`Start WS server on the ${address.port} port!`);
+});
