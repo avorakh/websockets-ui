dd4da1836fadb8220ccf06e913ef004a790850e0
Aliaksandr Vorakh <aliaksandrvorakh@gmail.com>
2024-10-25 23:14:34 +0200

    refactor: modify log messages

--- src/ws_server/index.ts
+++ src/ws_server/index.ts
@@ -7,9 +7,11 @@ const ws_server = new WebSocketServer({ port: Number(PORT) });
 ws_server.on('connection', (ws: WebSocket) => {
     console.log('Client connected');
 
-    ws.on('message', (message: string) => {
-        console.log(`Received message: ${message}`);
-        //TBA message handling
+    ws.on('message', (command: string) => {
+        console.log(`Received command: ${command}`);
+        const result = command;
+        console.log(`Result: ${result}`);
+        ws.send(result);
     });
 
     ws.on('close', () => {
@@ -18,6 +20,14 @@ ws_server.on('connection', (ws: WebSocket) => {
 
     ws.on('error', (error) => {
         console.error(`WebSocketServer error: ${error}`);
+    });
+});
+
+process.on('SIGINT', () => {
+    console.log('Shutting down WS server...');
+    ws_server.close(() => {
+        console.log('WS server closed');
+        process.exit(0);
     });
 });
 
